1807c85993ceefe8572af3d10068ad0728eea058
sata21 <sata21@163.com>
2020-06-23 16:56:48 +0800

    新建工程

---
+++ app/src/main/java/com/example/git_demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.git_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text);//添加文字
+    }
+}
