9ad7f1d35e9593ae653d3e5c5dc5a5b9097fdf07
sata21 <sata21@163.com>
2020-06-23 17:51:43 +0800

    添加WebView

--- app/src/main/java/com/example/git_demo/MainActivity.java
+++ app/src/main/java/com/example/git_demo/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.git_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text=findViewById(R.id.text);//添加文字
+        text = findViewById(R.id.text);//添加文字
+        webView = findViewById(R.id.webview);
+        webView.loadUrl("https://www.baidu.com");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //使用WebView加载显示url
+                view.loadUrl(url);
+                //返回true
+                return true;
+            }
+        });
         //测试拉取
         //测试冲突222
         //修改密码测试
